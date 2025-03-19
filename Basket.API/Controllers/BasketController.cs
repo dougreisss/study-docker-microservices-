40af122f9fc6caded88b9fac6b1c2fa7a0aa20cb
dougreisss <douglas_rrs@hotmail.com>
2025-03-19 17:53:10 -0300

    :sparkles: feat: discount api

--- Basket.API/Controllers/BasketController.cs
+++ Basket.API/Controllers/BasketController.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Basket.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/[controller]")]
     [ApiController]
     public class BasketController : ControllerBase
     {
